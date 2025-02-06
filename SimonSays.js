4fcfb8406999a040af6839496e9111ab76cd6a7f
Kundan-Kumar9 <kundankunal9546pk@gmail.com>
2025-02-06 11:41:24 +0530

    Initial commit

---
+++ SimonSays.js
@@ -0,0 +1,111 @@
+let gameSeq = [];
+let userSeq = [];
+
+// console.log(highScore);
+
+let started = false;
+let level = 0;
+let btns = ["yellow", "red", "green", "purple"];
+let isNameSet = true;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", () => {
+    if (started == false) {
+        console.log("Game is started");
+        started = true;
+
+        levelUp();
+    }
+})
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    setHighScore(level);
+    // let highScore = gameSeq.length;
+    // console.log(highScore);
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randomBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randomBtn);
+    
+}
+
+function setHighScore(level){
+    // localStorage.clear()
+    const user = JSON.parse(localStorage.getItem("user"));
+    // console.log(user)
+    if(user==null || level > user.highScore){
+        let inp;
+        if(isNameSet){
+           inp = prompt("Congrats! You break High Score, Enter Your name");
+           isNameSet = false;
+        }
+        const user = {"highScore":level,"Name":inp}
+        localStorage.setItem("user",JSON.stringify(user));
+    }
+}
+
+function checkAns(idx) {
+    // console.log(`Current level is: ${level}`);
+
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 170);
+        }
+    } else {
+        document.body.style.backgroundColor = "red"
+        setTimeout(function () {
+            document.body.style.backgroundColor = "white";
+        }, 200);
+
+        const user = JSON.parse(localStorage.getItem("user"));
+        h2.innerHTML = `Game Over! Your score was <b>${level}<b> <br> High Score is ${user.highScore} <br>Created by ${user.Name} <br>Press Any Key to Start.`;
+        reset();
+    }
+}
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 170);
+}
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 170);
+}
+
+function btnPress() {
+    let btn = this;
+    // console.log(this);
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    console.log(userSeq);
+
+    checkAns(userSeq.length - 1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    gameSeq = [];
+    userSeq = [];
+
+    started = false;
+    level = 0;
+    isNameSet = true;
+}
